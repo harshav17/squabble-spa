21b90626556a5cb8babbefe59e554e87f7d99d7c
harsha <harshav17@gmail.com>
2024-01-19 00:12:56 -0800

    add more tests

--- app/ui/expenses/__tests__/table.test.tsx
+++ app/ui/expenses/__tests__/table.test.tsx
@@ -7,8 +7,8 @@ jest.mock('@/app/lib/data');
 
 describe('ExpensesTable', () => {
   const mockExpenses = [
-    { expense_id: 1, timestamp: '2022-01-01', amount: 100, description: 'Test 1', paid_by_user: { name: 'User 1' } },
-    { expense_id: 2, timestamp: '2022-01-02', amount: 200, description: 'Test 2', paid_by_user: { name: 'User 2' } },
+    { expense_id: 1, timestamp: '2022-01-01T10:00:54Z', amount: 100, description: 'Test 1', paid_by_user: { name: 'User 1' } },
+    { expense_id: 2, timestamp: '2022-01-02T10:00:54Z', amount: 200, description: 'Test 2', paid_by_user: { name: 'User 2' } },
     // Add more mock data as needed
   ];
 
@@ -38,5 +38,17 @@ describe('ExpensesTable', () => {
     // test table data
     const data = screen.getAllByRole('cell');
     expect(data).toHaveLength(10);
+
+    // test row 0
+    expect(data[0]).toHaveTextContent('Jan 1, 2022');
+    expect(data[1]).toHaveTextContent('$100.00');
+    expect(data[2]).toHaveTextContent('Test 1');
+    expect(data[3]).toHaveTextContent('User 1');
+
+    // test row 1
+    expect(data[5]).toHaveTextContent('Jan 2, 2022');
+    expect(data[6]).toHaveTextContent('$200.00');
+    expect(data[7]).toHaveTextContent('Test 2');
+    expect(data[8]).toHaveTextContent('User 2');
   });
 });
